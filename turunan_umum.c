c7c7ead56b82efa88af002dab84dc9f4951b5fa3
Martinus William Hartono <martinus.wh@gmail.com>
2020-04-09 00:57:47 +0700

    second commit

--- turunan_umum.c
+++ turunan_umum.c
@@ -73,6 +73,8 @@ int main(void)
     double Vi[1000], Vo[1000], t=0;
 
     FILE *fp1, *fp2;
+    fp1 = fopen("input_voltage.csv", "w+");
+    fp2 = fopen("output_voltage.csv", "w+");
 
     printf("-----PROGRAM SIMULASI RANGKAIAN OP-AMP DIFFERENSIATOR OLEH KELOMPOK 2-----\n");
     printf("Program merupakan program simulasi rangkaian op-amp differentiator orde 1\n");
@@ -82,7 +84,136 @@ int main(void)
     printf("User juga akan diminta untuk menginput amplitudo dan periode masing-masing gelombang.\n");
     printf("Sebagai output, program akan menampilkan grafik tegangan input dan output.\n\n");
 
-    printf("Silakan masukkan nilai resistansi resistor (dalam ohm): "); scanf("%lf", &R);
+    while(quit==0)
+    {
+        printf("Silakan masukkan nilai resistansi resistor (dalam ohm): "); scanf("%lf", &R);
+        printf("Silakan masukkan nilai kapasitansi kapasitor (dalam Farrat): "); scanf("%lf", &C);
+        printf("Silakan masukkan periode gelombang: "); scanf("%lf", &T);
+        printf("Silakan masukkan amplitudo gelombang: "); scanf("%lf", &A);
+
+        printf("\nPilih jenis gelombang input:\n");
+        printf("1. Sinus\n");
+        printf("2. Cosinus\n");
+        printf("3. Segitiga\n");
+        printf("4. Kotak\n");
+        printf("5. Keluar dari program\n");         //not implemented
+
+        printf("\nSilakan masukkan jenis gelombang tegangan input: "); scanf("%d", &choice);
+
+        if(choice==5)
+        {
+            quit=1;
+        };
+
+        while((choice!=5) && (quit!=1))
+        {
+            if(choice==1)               //sinus
+            {
+                printf("Gelombang yang dipilih merupakan gelombang sinus, tegangan input dan output akan muncul pada file.csv\n");
+                Vi[0]=0;
+                angular_freq= 2.0*pi/T;
+                fprintf(fp1, "%f,", 0);
+                fprintf(fp1, "%f\n", Vi[0]);
+
+                for(i=1;i<1000;i++)
+                {
+                    t=t+dt;
+                    Vi[i] = A*sin(angular_freq*t);
+                    printToFile(fp1, i, Vi[i]);
+                };
+
+                t=0;
+                for(i=0;i<1000;i++)
+                {
+                    Vo[i] = A*deriveTrig(sin, angular_freq*t);
+                    printToFile(fp2, i, Vo[i]);
+                    t=t+dt;
+                };
+            }
+            else if(choice==2)          //cosinus
+            {
+                printf("Gelombang yang dipilih merupakan gelombang cosinus, tegangan input dan output akan muncul pada file.csv\n");
+                Vi[0]=0;
+                angular_freq= 2.0*pi/T;
+                fprintf(fp1, "%f,", 0);
+                fprintf(fp1, "%f\n", Vi[0]);
+
+                for(i=1;i<1000;i++)
+                {
+                    t=t+dt;
+                    Vi[i] = A*cos(angular_freq*t);
+                    printToFile(fp1, i, Vi[i]);
+                };
+
+                t=0;
+                for(i=0;i<1000;i++)
+                {
+                    Vo[i] = A*deriveTrig(cos, angular_freq*t);
+                    printToFile(fp2, i, Vo[i]);
+                    t=t+dt;
+                };
+            }
+            else if(choice==3)          //segitiga
+            {
+                printf("Gelombang yang dipilih merupakan gelombang segitiga, tegangan input dan output akan muncul pada file.csv\n");
+                Vi[0]=0;
+                fprintf(fp1, "%f,", 0);
+                fprintf(fp1, "%f\n", Vi[0]);
+
+                for(i=1;i<1000;i++)
+                {
+                    t=t+dt;
+                    Vi[i] = fungsi1(t, T,A);
+                    printToFile(fp1, i, Vi[i]);
+                };
+
+                t=0;
+                for(i=0;i<1000;i++)
+                {
+                    Vo[i] = derive(fungsi1, t, R, C);
+                    printToFile(fp2, i, Vo[i]);
+                    t=t+dt;
+                };
+            }
+            else if(choice==4)          //kotak
+            {
+                printf("Gelombang yang dipilih merupakan gelombang kotak, tegangan input dan output akan muncul pada file.csv\n");
+                Vi[0]=A;
+                fprintf(fp1, "%f,", 0);
+                fprintf(fp1, "%f\n", Vi[0]);
+
+                for(i=1;i<1000;i++)
+                {
+                    t=t+dt;
+                    Vi[i] = fungsi2(t, T,A);
+                    printToFile(fp1, i, Vi[i]);
+                };
+
+                t=0;
+                for(i=0;i<1000;i++)
+                {
+                    Vo[i] = derive(fungsi2, t, R, C);
+                    printToFile(fp2, i, Vo[i]);
+                    t=t+dt;
+                };
+            }
+            else        //input choice ulang
+            {
+                printf("Input salah silakan input ulang.\n");
+            };
+            printf("\nSilakan masukkan jenis gelombang tegangan input: "); scanf("%d", &choice);
+        };
+
+        printf("Apakah user ingin keluar dari program atau melakukan simulasi dengan nilai komponen lain? (1/0): "); scanf("%d", &quit);
+
+
+
+
+        fclose(fp1);
+        fclose(fp2);
+    }
+
+    /*printf("Silakan masukkan nilai resistansi resistor (dalam ohm): "); scanf("%lf", &R);
     printf("Silakan masukkan nilai kapasitansi kapasitor (dalam Farrat): "); scanf("%lf", &C);
     printf("Silakan masukkan periode gelombang: "); scanf("%lf", &T);
     printf("Silakan masukkan amplitudo gelombang: "); scanf("%lf", &A);
@@ -194,5 +325,7 @@ int main(void)
     double der=derive(sin, 0, R, C);
     printf("\n\n%f\n", der);
 
+    fclose(fp1);
+    fclose(fp2);*/
     return 0;
 };
